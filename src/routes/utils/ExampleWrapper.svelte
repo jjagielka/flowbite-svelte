ea2644c4fa8fcc8cc3265acb89b842774080047c
Jakub Jagielka <jjagielka@gmail.com>
2025-02-26 17:26:21 +0100

    TS types at Theme.svelte

--- src/routes/utils/ExampleWrapper.svelte
+++ src/routes/utils/ExampleWrapper.svelte
@@ -8,49 +8,13 @@
   });
 </script>
 
-<div class="root">
-  <div class="container">
-    <div class="example-wrapper bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700">
-      <div class="example">
+<div class="my-2">
+  <div class="flex flex-col w-full overflow-hidden rounded-b-lg">
+    <div class="flex overflow-hidden rounded-t-lg bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700">
+      <div class={['w-full mx-auto bg-linear-to-r bg-white dark:bg-gray-900 p-6', meta.class]}>
         {@render example?.()}
       </div>
     </div>
-    <pre class="language-svelte p-4">{@render code?.()}</pre>
+    <pre class="language-svelte text-sm p-4">{@render code?.()}</pre>
   </div>
 </div>
-
-<style>
-  .root {
-    margin: 0.5rem 0;
-  }
-
-  .container {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    overflow: hidden;
-    border-bottom-left-radius: var(--example-border-radius, 0.25rem);
-    border-bottom-right-radius: var(--example-border-radius, 0.25rem);
-  }
-
-  .example-wrapper {
-    display: flex;
-    justify-content: center;
-    overflow: hidden;
-
-    border-top-left-radius: var(--example-border-radius, 0.25rem);
-    border-top-right-radius: var(--example-border-radius, 0.25rem);
-  }
-
-  .example {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0.75rem 0;
-    min-height: 13rem;
-  }
-
-  pre {
-    margin: 0;
-  }
-</style>
